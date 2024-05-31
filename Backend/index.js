c66cac3c89ce130ae263f1b7e38573678eb9d69a
Rishi Kashyap <kashyap1strishi@gmail.com>
2024-05-31 21:16:19 +0530

    index commit

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
 
 app.use(cors({
-    origin: "https://learnittt.netlify.app",
+    origin: "https://learniitt.netlify.app/",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
