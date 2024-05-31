f21e5a8a915303ca4e197bd135096e32e43c4106
Rishi Kashyap <kashyap1strishi@gmail.com>
2024-05-31 20:49:19 +0530

    CORS Updates

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
 
 app.use(cors({
-    origin: "http://localhost:5173", // replace with your frontend URL
+    origin: "https://learnittt.netlify.app/",
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
